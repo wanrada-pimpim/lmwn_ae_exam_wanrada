b9376bc712020362a7b656ca060d37105b00e46d
wanrada-pimpim <pimpim1818@gmail.com>
2025-04-29 22:33:16 +0700

    Fix model datamart fleet_management_order  by adding more columns used in report, and remove joining with support_tickets

--- models/datamart/fleet_management/model_datamart_fleet_management_order.sql
+++ models/datamart/fleet_management/model_datamart_fleet_management_order.sql
@@ -3,17 +3,16 @@ SELECT
     drivers_master.region,
     drivers_master.vehicle_type,
     drivers_master.driver_rating,
+    drivers_master.join_date,
+    drivers_master.active_status AS driver_status,
     order_transactions.order_id,
     order_transactions.order_status,
     order_transactions.delivery_zone,
+    order_transactions.order_datetime,
     order_transactions.pickup_datetime,
     order_transactions.delivery_datetime,
     date_diff('minute', order_transactions.pickup_datetime, order_transactions.delivery_datetime) AS delivery_minute,
-    order_transactions.is_late_delivery,
-    support_tickets.issue_type,
-    support_tickets.issue_sub_type
+    order_transactions.is_late_delivery
 FROM main.drivers_master
 LEFT JOIN main.order_transactions 
 ON drivers_master.driver_id = order_transactions.driver_id
-LEFT JOIN main.support_tickets 
-ON order_transactions.order_id = support_tickets.order_id
